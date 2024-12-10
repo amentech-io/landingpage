e35b2a06bbcc04beeb3288f3ae73c67102c875a1
zirbest <zirbst@gmail.com>
2024-12-10 20:55:05 +0100

    chore: app features - add animated paths

---
+++ app/components/AppFeatures.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="mx-auto mt-28 max-w-[1530px]">
+    <div class="grid">
+      <div class="z-10 col-start-1 row-start-1">
+        <h3 class="mx-auto mt-20 max-w-[32ch] text-center text-5xl font-bold text-white">Les Fonctionnalités Clés qui Transforment Vos Opérations</h3>
+      </div>
+      <div class="col-start-1 row-start-1">
+        <FeaturesSvg />
+      </div>
+    </div>
+  </section>
+</template>
